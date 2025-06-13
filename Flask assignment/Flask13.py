d4c5aa54905071d0cd3b8a7717d1fed995bba495
vaibhavkumar39 <vaibhavkumar39.00@gmail.com>
2025-06-13 14:45:42 +0530

    Update Flask13.py

--- Flask assignment/Flask13.py
+++ Flask assignment/Flask13.py
@@ -1,25 +1,2 @@
 #13. Implement notifications in a Flask app using websockets to notify users of updates.
 
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode='threading')  
-
-@app.route('/')
-def index():
-    return render_template('notify.html')
-
-@app.route('/send_notification', methods=['POST'])
-def send_notification():
-    message = request.form.get('message', 'New Notification!')
-    socketio.emit('notification', {'message': message})
-    return 'Sent!', 200
-
-@socketio.on('connect')
-def handle_connect():
-    print('User connected')
-
-if __name__ == '__main__':
-    socketio.run(app, debug=True)
